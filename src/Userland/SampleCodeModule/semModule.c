96abaa690180df5e363f09020beb397bae17548c
mtarradellas <mtarradellas@itba.edu.ar>
2019-05-14 01:48:14 -0300

    yafu

--- src/Userland/SampleCodeModule/semModule.c
+++ src/Userland/SampleCodeModule/semModule.c
@@ -4,7 +4,7 @@
 #include "stdlib.h"
 
 void semOpen(char id[MAX_SEM_ID], int start) {
-  systemCall((uint64_t)SEMOPEN, (uint64_t)id, (uint64_t)&start, 0, 0, 0);
+  systemCall((uint64_t)SEMOPEN, (uint64_t)id, (uint64_t)start, 0, 0, 0);
 }
 void semClose(char id[MAX_SEM_ID]) {
   systemCall((uint64_t)SEMCLOSE, (uint64_t)id, 0, 0, 0, 0);
