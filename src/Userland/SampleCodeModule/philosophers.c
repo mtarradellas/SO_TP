f24014bebb4fc069ccb464ad3904d5bec68782e4
Santiago Reyes <sreyes@itba.edu.ar>
2019-06-01 18:41:04 -0300

    working philosophers

--- src/Userland/SampleCodeModule/philosophers.c
+++ src/Userland/SampleCodeModule/philosophers.c
@@ -22,10 +22,13 @@ static void philosopherSelfdestruct();
 static void philosophersKillAll();
 static void printStatus();
 
-int philosophersQty, philosophersToDie, id, indexToDie;
-philosopher_t philosophers[MAX_PHILOSOPHERS] = {{0}};
-queue_t thinkingList, eatingList;
-
+static int philosophersQty;
+// static int philosophersToDie;
+static int killing;
+static int id, indexToDie;
+static philosopher_t philosophers[MAX_PHILOSOPHERS] = {{0}};
+static queue_t thinkingList, eatingList;
+static char action[50];
 void philosophersRun() {
   printMenu();
   mutexOpen("philosophers");
@@ -33,10 +36,10 @@ void philosophersRun() {
   philosophersQty = 0;
   semOpen("forks", 0);
   semOpen("inTable", 0);
-  philosophersToDie = 0;
+  // philosophersToDie = 0;
+  killing = 0;
   id = 0;
   indexToDie = 0;
-
   thinkingList = queueCreate(sizeof(int));
   eatingList = queueCreate(sizeof(int));
 
@@ -53,7 +56,6 @@ void philosophersRun() {
         break;
     }
   }
-
   philosophersKillAll();
   mutexClose("philosophers");
   mutexClose("eating");
@@ -84,7 +86,7 @@ static void philosopherCreate() {
   phi.pid =
       createProcess("philosopher", (mainf)philosopher, index, NULL, HIGHP);
   phi.id = index;
-  indexToDie = id - 1;
+  indexToDie = index;
   philosophers[philosophersQty++] = phi;
   if (philosophersQty > 1) semPost("inTable");
   mutexUnlock("philosophers");
@@ -104,27 +106,30 @@ static int philosopher(int argc, char** argv) {
   int i = argc;
 
   while (1) {
-    if (philosophersToDie > 0 && i == indexToDie) {
+    wait(1);
+    mutexLock("philosophers");
+    if (killing /*philosophersToDie == 0*/ && i == indexToDie) {
+      mutexUnlock("philosophers");
       philosopherSelfdestruct();
     }
-
+    mutexUnlock("philosophers");
     semWait("inTable");
     semWait("forks");
     semWait("forks");
 
     mutexLock("eating");
-    queueRemove(thinkingList, cmp, &i);
+    queueRemove(thinkingList, &cmp, &i);
     queueOffer(eatingList, &i);
     printStatus();
     mutexUnlock("eating");
 
-    wait(20);
+    wait(30);
     semPost("forks");
     semPost("forks");
     semPost("inTable");
 
     mutexLock("eating");
-    queueRemove(eatingList, cmp, &i);
+    queueRemove(eatingList, &cmp, &i);
     queueOffer(thinkingList, &i);
     mutexUnlock("eating");
   }
@@ -138,25 +143,49 @@ static void philosopherKill() {
     mutexUnlock("philosophers");
     return;
   }
-  philosophersToDie++;
+  if (philosophersQty == 1) {
+    printf(
+        "please create at least 2 philosophers before starting "
+        "to kill them\n\n");
+    mutexUnlock("philosophers");
+    return;
+  }
+  // if (philosophersToDie < philosophersQty - 1) {
+  //   philosophersToDie++;
+  //   printf("asked %d philosopher(s) to kindly die :)\n\n",
+  //   philosophersToDie);
+  // } else {
+  //   printf(
+  //       "you sent too many death requests, wait for some philosophers to die
+  //       " "or make some more philosophers before sending more\n");
+  // }
+
+  if (killing == 0) {
+    killing = 1;
+    printf("\nasked a philosopher to kill himself >:)\n\n");
+  } else {
+    printf("\nwait for the philosopher to die before killing another one\n");
+  }
+
   mutexUnlock("philosophers");
 }
 
 static void philosopherSelfdestruct() {
   mutexLock("philosophers");
   philosophersQty--;
-  philosophersToDie--;
+  killing = 0;
+  // philosophersToDie--;
   indexToDie = (philosophers[philosophersQty - 1]).id;
-  int phiToDie = (philosophers[philosophersQty]).pid;
-
+  philosopher_t philosopherToDie = philosophers[philosophersQty];
   mutexUnlock("philosophers");
   semWait("inTable");
   semWait("forks");
-  printf("\nphilosopher %d has died :(\n\n", (philosophers[philosophersQty]).id);
   mutexLock("eating");
-  queueRemove(thinkingList, cmp, &(philosophers[philosophersQty]).id);
+  queueRemove(thinkingList, &cmp, &(philosophers[philosophersQty]).id);
+  queueRemove(eatingList, &cmp, &(philosophers[philosophersQty]).id);
   mutexUnlock("eating");
-  kill(phiToDie);
+  printf("philosopher %d has died :(\n\n", philosopherToDie.id);
+  kill(philosopherToDie.pid);
 }
 
 static void philosophersKillAll() {
@@ -170,26 +199,38 @@ static void philosophersKillAll() {
 
   mutexUnlock("philosophers");
 }
-static void printList(queue_t queue) {
+static int printList(queue_t queue) {
+  int usedSpaces = 0;
   queueResetIter(queue);
   int next;
   while (queueGetNext(queue, &next) == 0) {
-    printf("%d-", next);
+    if (next < 10) {
+      printf("%d  ", next);
+    } else {
+      printf("%d ", next);
+    }
+    usedSpaces += 3;
   }
-  return;
+  return usedSpaces;
 }
 static void printStatus() {
-  int spacesE = queueSize(eatingList);
-
   printf("Eating: ");
-  printList(eatingList);
+  int maxSpaces = (3 * MAX_PHILOSOPHERS / 2) + 1;
+  int usedSpaces = printList(eatingList);
 
-  for (int i = 0; i < (3 * MAX_PHILOSOPHERS / 2) - spacesE; i++) {
+  for (int i = 0; i < maxSpaces - usedSpaces; i++) {
     printf(" ");
   }
 
   printf("Thinking: ");
-  printList(thinkingList);
+  usedSpaces = printList(thinkingList);
+  maxSpaces = 3 * (MAX_PHILOSOPHERS - 3);
 
+  for (int i = 0; i < maxSpaces - usedSpaces; i++) {
+    printf(" ");
+  }
+
+  printf("Total: %d", philosophersQty);
   printf("\n");
+  wait(1);
 }
